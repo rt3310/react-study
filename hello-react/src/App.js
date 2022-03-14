0a4f6ece6c2bdbb498b1a3ea64d79fcbd57bc8a4
rt3310 <btac3310@gmail.com>
2022-03-14 21:55:41 +0900

    update

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+import EventPractice from "./EventPractice";
+import IterationSample from "./IterationSample";
+import MyComponent from "./MyComponent";
+import Say from "./Say";
+import ScrollBox from "./ScrollBox";
+import ValidationSample from "./ValidationSample";
+
+class App extends Component {
+  render() {
+    return (
+      <IterationSample />
+      
+    );
+  }
+};
+
+export default App;
